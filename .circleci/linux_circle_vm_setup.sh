31311214a52a3193974ccabec4a42a8f4d3cc223
Randy Fay <randy@randyfay.com>
2021-10-09 07:38:39 -0600

    Update ubuntu runner and remove special qemu stuff

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -5,7 +5,7 @@ set -x
 
 sudo apt-get -qq update && sudo apt-get -qq install -y docker-ce-cli binfmt-support qemu-user-static
 
-BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-amd64"
+BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-amd64"
 
 curl --output docker-buildx \
   --silent --show-error --location --fail --retry 3 \
@@ -15,11 +15,5 @@ mkdir -p ~/.docker/cli-plugins
 mv docker-buildx ~/.docker/cli-plugins
 chmod a+x ~/.docker/cli-plugins/docker-buildx
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-if ! docker buildx inspect ddev-builder-multi --bootstrap >/dev/null; then docker buildx create --name ddev-builder-multi; fi
-docker buildx use ddev-builder-multi
+if ! docker buildx inspect ddev-builder-multi --bootstrap >/dev/null; then docker buildx create --name ddev-builder-multi --use; fi
 docker buildx inspect --bootstrap
-
-which dpkg-split
-
