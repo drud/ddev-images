f23d6c562bb50c081e88bc53693275cf0be82ceb
Randy Fay <randy@randyfay.com>
2021-10-15 16:42:18 -0600

    Update ubuntu runner and remove special qemu stuff (#60)
    * Update ubuntu runner and remove special qemu stuff
    * Upgrade qemu and buildx to solve qemu segfault

--- .circleci/linux_circle_vm_setup.sh
+++ .circleci/linux_circle_vm_setup.sh
@@ -3,23 +3,18 @@
 set -eu -o pipefail
 set -x
 
-sudo apt-get -qq update && sudo apt-get -qq install -y docker-ce-cli binfmt-support qemu-user-static
+# Get recent qemu to avoid constant qemu crashes on Ubuntu 20.04
+# Incomprehensible discussions of the problem at
+# https://bugs.launchpad.net/ubuntu/+source/qemu/+bug/1928075
+sudo add-apt-repository ppa:jacob/virtualisation
 
-BUILDX_BINARY_URL="https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-amd64"
+sudo apt-get -qq update && sudo apt-get -qq install -y docker-ce-cli binfmt-support  qemu qemu-user qemu-user-static
 
-curl --output docker-buildx \
-  --silent --show-error --location --fail --retry 3 \
-  "$BUILDX_BINARY_URL"
 
-mkdir -p ~/.docker/cli-plugins
-mv docker-buildx ~/.docker/cli-plugins
-chmod a+x ~/.docker/cli-plugins/docker-buildx
+# Get recent buildx
+mkdir -p ~/.docker/cli-plugins && curl -sSL -o ~/.docker/cli-plugins/docker-buildx https://github.com/docker/buildx/releases/download/v0.6.3/buildx-v0.6.3.linux-amd64 && chmod +x ~/.docker/cli-plugins/docker-buildx
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx version
 
-if ! docker buildx inspect ddev-builder-multi --bootstrap >/dev/null; then docker buildx create --name ddev-builder-multi; fi
-docker buildx use ddev-builder-multi
+if ! docker buildx inspect ddev-builder-multi --bootstrap >/dev/null; then docker buildx create --name ddev-builder-multi --use; fi
 docker buildx inspect --bootstrap
-
-which dpkg-split
-
